c86aab48e57d22486119d209d5ee97560c453caa
Pawel Strzelewicz <@>
2022-05-01 19:43:57 +0200

    Add aviation_gis_tools library

---
+++ aviation_lon_lat_calculator/aviation_gis_tools/angle.py
@@ -0,0 +1,243 @@
+from math import fabs
+from typing import Tuple, Union
+import re
+
+# Angle types
+AT_LONGITUDE = 'AT_LONGITUDE'
+AT_LATITUDE = 'AT_LATITUDE'
+AT_BEARING = 'AT_BEARING'
+
+# Angle formats
+AF_DMSH_SPACE_SEP = 'AF_DMSH_SPACE_SEP'  # e.g.: 55 22 43.47N
+AF_DMSH_HYPHEN_SEP = 'AF_DMSH_HYPHEN_SEP'  # e.g.: 55-2-43.47N
+AF_HDMS_SPACE_SEP = 'AF_HDMS_SPACE_SEP'  # e.g.: N55 22 43.47
+AF_HDMS_HYPHEN_SEP = 'AF_HDMS_HYPHEN_SEP'  # e.g.: N55-22-43.47
+
+# Angle string representation formats
+ANGLE_FORMAT_PATTERNS = {
+    AT_LONGITUDE: {
+        AF_DMSH_SPACE_SEP: '{d:03d} {m:02d} {s:0{sec_length}.{sec_prec}f}{h}',
+        AF_DMSH_HYPHEN_SEP: '{d:03d}-{m:02d}-{s:0{sec_length}.{sec_prec}f}{h}',
+        AF_HDMS_SPACE_SEP: '{h}{d:03d} {m:02d} {s:0{sec_length}.{sec_prec}f}',
+        AF_HDMS_HYPHEN_SEP: '{h}{d:03d}-{m:02d}-{s:0{sec_length}.{sec_prec}f}',
+    },
+    AT_LATITUDE: {
+        AF_DMSH_SPACE_SEP: '{d:02d} {m:02d} {s:0{sec_length}.{sec_prec}f}{h}',
+        AF_DMSH_HYPHEN_SEP: '{d:02d}-{m:02d}-{s:0{sec_length}.{sec_prec}f}{h}',
+        AF_HDMS_SPACE_SEP: '{h}{d:02d} {m:02d} {s:0{sec_length}.{sec_prec}f}',
+        AF_HDMS_HYPHEN_SEP: '{h}{d:02d}-{m:02d}-{s:0{sec_length}.{sec_prec}f}',
+    }
+}
+
+
+class Angle:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def normalize_angle(ang_src: str) -> str:
+        """ Normalize angle, e. g. replace ',' with '.', ensure cardinal directions (N, S etc.) are capital etc.
+        :param ang_src: str, angle source valueb
+        >>> assert Angle.normalize_angle(' 32   44 56,77n') == '32 44 56.77N'
+        """
+        norm_ang = ang_src.strip().upper().replace(',', '.')
+        norm_ang = re.sub(r'\s+', ' ', norm_ang)
+        return norm_ang
+
+    @staticmethod
+    def is_angle_within_range(ang_dd: float, ang_type: str) -> Union[bool, None]:
+        """  Check if coordinate is within range for specified angle type
+        :param ang_dd: float, coordinate to check
+        :param ang_type: const(str): type of angle
+        >>> assert  Angle.is_angle_within_range(-180.001, AT_LONGITUDE) is False
+        >>> assert  Angle.is_angle_within_range(-180.00, AT_LONGITUDE) is True
+        >>> assert  Angle.is_angle_within_range(-179.999, AT_LONGITUDE) is True
+        >>> assert  Angle.is_angle_within_range(179.999, AT_LONGITUDE) is True
+        >>> assert  Angle.is_angle_within_range(180.00, AT_LONGITUDE) is True
+        >>> assert  Angle.is_angle_within_range(180.001, AT_LONGITUDE) is False
+        >>> assert  Angle.is_angle_within_range(-90.001, AT_LATITUDE) is False
+        >>> assert  Angle.is_angle_within_range(-90.00, AT_LATITUDE) is True
+        >>> assert  Angle.is_angle_within_range(-89.999, AT_LATITUDE) is True
+        >>> assert  Angle.is_angle_within_range(89.999, AT_LATITUDE) is True
+        >>> assert  Angle.is_angle_within_range(90.00, AT_LATITUDE) is True
+        >>> assert  Angle.is_angle_within_range(90.001, AT_LATITUDE) is False
+        >>> assert  Angle.is_angle_within_range(-0.001, AT_BEARING) is False
+        >>> assert  Angle.is_angle_within_range(0.000, AT_BEARING) is True
+        >>> assert  Angle.is_angle_within_range(0.001, AT_BEARING) is True
+        >>> assert  Angle.is_angle_within_range(359.999, AT_BEARING) is True
+        >>> assert  Angle.is_angle_within_range(360.000, AT_BEARING) is True
+        >>> assert  Angle.is_angle_within_range(360.001, AT_BEARING) is False
+        >>> Angle.is_angle_within_range(123, 'AT_NONE')
+        Traceback (most recent call last):
+        ...
+        ValueError: Angle type AT_NONE is not supported.
+
+        """
+        if ang_type == AT_LONGITUDE:
+            return -180 <= ang_dd <= 180
+        elif ang_type == AT_LATITUDE:
+            return -90 <= ang_dd <= 90
+        elif ang_type == AT_BEARING:
+            return 0 <= ang_dd <= 360
+        else:
+            raise ValueError(f'Angle type {ang_type} is not supported.')
+
+
+    @staticmethod
+    def dd_to_dms_parts(ang_dd: float, prec: int = 3) -> Tuple[int, int, float]:
+        """ Extract degrees, minutes, seconds from angle in DD (decimal degrees) format.
+        >>> assert Angle.dd_to_dms_parts(0) == (0, 0, 0.000)
+        >>> assert Angle.dd_to_dms_parts(-1) == (-1, 0, 0.000)
+        >>> assert Angle.dd_to_dms_parts(10) == (10, 0, 0.000)
+        >>> assert Angle.dd_to_dms_parts(-45.5) == (-45, 30, 0.000)
+        >>> assert Angle.dd_to_dms_parts(1.0169444444444400) == (1, 1, 1.0)
+        >>> assert Angle.dd_to_dms_parts(100.1694444444444000) == (100, 10, 10.000)
+        """
+        minutes, seconds = divmod(fabs(ang_dd) * 3600, 60)
+        degrees, minutes = divmod(minutes, 60)
+        seconds = round(seconds, prec)
+
+        # Ensure that rounded seconds with specified precision are not >= 60
+        if seconds >= 60:
+            seconds = 0.0
+            minutes += 1
+        if minutes >= 60:
+            minutes = 0
+            degrees += 1
+
+        if ang_dd < 0:
+            degrees = -degrees
+
+        return int(degrees), int(minutes), seconds
+
+    @staticmethod
+    def get_hemisphere_designator(ang_dd: float, ang_type: str) -> str:
+        """ Get hemisphere designator e.g. S, N base on "sign" (positive/negative) of angle in DD format and angle type.
+        :param ang_dd: angle in DD format
+        :param ang_type: angle type
+        :return: str: hemisphere designator: N, E, S or W
+        >>> assert Angle.get_hemisphere_designator(-123.44, AT_LONGITUDE) == 'W'
+        >>> assert Angle.get_hemisphere_designator(33, AT_LONGITUDE) == 'E'
+        >>> assert Angle.get_hemisphere_designator(23.44, AT_LATITUDE) == 'N'
+        >>> assert Angle.get_hemisphere_designator(-23.44, AT_LATITUDE) == 'S'
+        """
+        if ang_type == AT_LONGITUDE:
+            return 'E' if ang_dd >= 0 else 'W'
+        elif ang_type == AT_LATITUDE:
+            return 'N' if ang_dd >= 0 else 'S'
+
+    @staticmethod
+    def convert_dd_to_dms(ang_dd: float,
+                          ang_type: str,
+                          dms_format: str = AF_DMSH_SPACE_SEP,
+                          prec: int = 3) -> str:
+        """ Convert angle from DD format into DMS format
+        :param ang_dd: angle in DD format
+        :param ang_type: angle type
+        :param dms_format:desired format of angle in DMS format
+        :param prec:  int, positive number of decimal point of seconds
+        :return: angle in DMS format
+        >>> assert Angle.convert_dd_to_dms(145.9589599661111000,
+        ...                                AT_LONGITUDE, dms_format=AF_HDMS_SPACE_SEP) == 'E145 57 32.256'
+        >>> assert Angle.convert_dd_to_dms(-145.9589599661111000,
+        ...                                AT_LONGITUDE, dms_format=AF_HDMS_HYPHEN_SEP) == 'W145-57-32.256'
+        >>> assert Angle.convert_dd_to_dms(145.9589599661111000,
+        ...                                 AT_LONGITUDE, dms_format=AF_DMSH_SPACE_SEP, prec=2) == '145 57 32.26E'
+        >>> assert Angle.convert_dd_to_dms(145.9589599661111000,
+        ...                                AT_LONGITUDE, dms_format=AF_DMSH_HYPHEN_SEP, prec=1) == '145-57-32.3E'
+        >>> assert Angle.convert_dd_to_dms(45.9589599661111000,
+        ...                                AT_LATITUDE, dms_format=AF_HDMS_SPACE_SEP) == 'N45 57 32.256'
+        >>> assert Angle.convert_dd_to_dms(-45.9589599661111000,
+        ...                                AT_LATITUDE, dms_format=AF_HDMS_HYPHEN_SEP) == 'S45-57-32.256'
+        >>> assert Angle.convert_dd_to_dms(45.9589599661111000,
+        ...                                AT_LATITUDE, dms_format=AF_DMSH_SPACE_SEP, prec=2) == '45 57 32.26N'
+        >>> assert Angle.convert_dd_to_dms(-45.9589599661111000,
+        ...                                AT_LATITUDE, dms_format=AF_DMSH_HYPHEN_SEP, prec=1) == '45-57-32.3S'
+        """
+        d, m, s = Angle.dd_to_dms_parts(ang_dd, prec)
+        h = Angle.get_hemisphere_designator(ang_dd, ang_type)
+
+        if prec > 0:
+            sec_length = prec + 3
+        else:
+            sec_length = 2
+
+        dms_format_pattern = ANGLE_FORMAT_PATTERNS[ang_type][dms_format]
+        return dms_format_pattern.format(d=int(fabs(d)), m=m, s=s, sec_length=sec_length, sec_prec=prec, h=h)
+
+    @staticmethod
+    def dmsh_parts_to_dd(dmsh_parts: Tuple[int, int, float, str]) -> Union[float, None]:
+        r""" Convert coordinates parts into degrees minutes format.
+        Note: If angle is within range, example longitude <-180, 180> will be check in separated method
+        :param dmsh_parts: tuple of degrees (int), minutes (int), seconds (float) and hemisphere character (str)
+        :return: dd: float
+        >>> assert Angle.dmsh_parts_to_dd((100, 35, 44, 'N')) == 100.59555555555555
+        >>> assert Angle.dmsh_parts_to_dd((100, 35, 44, 'W')) == -100.59555555555555
+        >>> Angle.dmsh_parts_to_dd((100, 61, 59, 'W'))
+        Traceback (most recent call last):
+        ValueError: Minutes 61 or/and seconds 59 outside range <0, 60).
+        <BLANKLINE>
+        >>> Angle.dmsh_parts_to_dd((100, 0, 60, 'E'))
+        Traceback (most recent call last):
+        ValueError: Minutes 0 or/and seconds 60 outside range <0, 60).
+        <BLANKLINE>
+        >>> Angle.dmsh_parts_to_dd((100, -1, 0, 'S'))
+        Traceback (most recent call last):
+        ValueError: Minutes -1 or/and seconds 0 outside range <0, 60).
+        <BLANKLINE>
+        >>> Angle.dmsh_parts_to_dd((100, 5, 10, 'A'))
+        Traceback (most recent call last):
+        ValueError: Hemisphere designator A not allowed.
+        <BLANKLINE>
+        """
+        d, m, s, h = dmsh_parts
+
+        if (0 <= m < 60) and (0 <= s < 60):
+            dd = d + m / 60 + s / 3600
+            if h in ['W', 'S']:
+                return -dd
+            elif h in ['N', 'E']:
+                return dd
+            else:
+                raise ValueError(f'Hemisphere designator {h} not allowed.\n')
+        else:
+            raise ValueError(f'Minutes {m} or/and seconds {s} outside range <0, 60).\n')
+
+    @staticmethod
+    def dmh_parts_to_dd(dmh_parts: Tuple[int, float, str]) -> Union[float, None]:
+        """ Convert coordinates parts into degrees minutes format.
+        Note: If angle is within range, example longitude <-180, 180> will be check in separated method
+        :param dmh_parts: tuple of degrees (int), minutes (int), seconds (float) and hemisphere character (str)
+        :return: dd: float
+        >>> assert Angle.dmh_parts_to_dd((10, 35.123, 'N')) == 10.5853833333333333
+        >>> assert Angle.dmh_parts_to_dd((100, 35.123, 'W')) == -100.5853833333333333
+        >>> Angle.dmh_parts_to_dd((100, 61, 'W'))
+        Traceback (most recent call last):
+        ValueError: Minutes: 61 outside range <0, 60).
+        <BLANKLINE>
+        >>> Angle.dmh_parts_to_dd((100, 60, 'E'))
+        Traceback (most recent call last):
+        ValueError: Minutes: 60 outside range <0, 60).
+        <BLANKLINE>
+        >>> Angle.dmh_parts_to_dd((100, -1, 'S'))
+        Traceback (most recent call last):
+        ValueError: Minutes: -1 outside range <0, 60).
+        <BLANKLINE>
+        >>> Angle.dmh_parts_to_dd((100, 5, 'A'))
+        Traceback (most recent call last):
+        ValueError: Hemisphere designator A not allowed.
+        <BLANKLINE>
+        """
+        d, m, h = dmh_parts
+
+        if 0 <= m < 60:
+            dd = d + m / 60
+            if h in ['W', 'S']:
+                return -dd
+            elif h in ['N', 'E']:
+                return dd
+            else:
+                raise ValueError(f'Hemisphere designator {h} not allowed.\n')
+        else:
+            raise ValueError(f'Minutes: {m} outside range <0, 60).\n')
---
+++ aviation_lon_lat_calculator/aviation_gis_tools/coordinate.py
@@ -0,0 +1,157 @@
+from aviation_lon_lat_calculator.aviation_gis_tools.angle import (
+    AT_LONGITUDE,
+    AT_LATITUDE,
+    Angle
+)
+from typing import Union
+import re
+
+COORDINATE_COMPACTED = {
+    AT_LONGITUDE: {
+        'DMSH_COMPACTED': re.compile(r'''(?P<deg>^180|^1[0-7]\d|^0\d{2})
+                                         (?P<min>[0-5]\d)
+                                         (?P<sec>[0-5]\d\.\d+|[0-5]\d)
+                                         (?P<hem>[EW]$)
+                                      ''', re.VERBOSE),
+        'HDMS_COMPACTED': re.compile(r'''(?P<hem>^[EW])
+                                         (?P<deg>180|1[0-7]\d|0\d{2})
+                                         (?P<min>[0-5]\d)
+                                         (?P<sec>[0-5]\d\.\d+$|[0-5]\d$)          
+                                      ''', re.VERBOSE),
+        'DMH_COMPACTED': re.compile(r'''(?P<deg>^180|^1[0-7]\d|^0\d{2})
+                                        (?P<min>[0-5]\d\.\d+|[0-5]\d)
+                                        (?P<hem>[EW]$)
+                                    ''', re.VERBOSE),
+        'HDM_COMPACTED': re.compile(r'''(?P<hem>^[EW])
+                                        (?P<deg>180|1[0-7]\d|0\d{2})
+                                        (?P<min>[0-5]\d\.\d+$|[0-5]\d$)
+                                    ''', re.VERBOSE)
+    },
+    AT_LATITUDE: {
+        'DMSH_COMPACTED': re.compile(r'''(?P<deg>^90|^[0-8]\d)
+                                         (?P<min>[0-5]\d)
+                                         (?P<sec>[0-5]\d\.\d+|[0-5]\d)
+                                         (?P<hem>[NS]$)
+                                    ''', re.VERBOSE),
+        'HDMS_COMPACTED': re.compile(r'''(?P<hem>^[NS])
+                                         (?P<deg>90|[0-8]\d)
+                                         (?P<min>[0-5]\d)
+                                         (?P<sec>[0-5]\d\.\d+$|[0-5]\d$)
+                                     ''', re.VERBOSE),
+        'DMH_COMPACTED': re.compile(r'''(?P<deg>^90|^[0-8]\d)
+                                        (?P<min>[0-5]\d\.\d+|[0-5]\d)
+                                        (?P<hem>[NS]$)
+                                    ''', re.VERBOSE),
+        'HDM_COMPACTED': re.compile(r'''(?P<hem>^[NS])
+                                         (?P<deg>90|[0-8]\d)
+                                         (?P<min>[0-5]\d\.\d+$|[0-5]\d$)
+                                    ''', re.VERBOSE)
+    }
+}
+
+
+class Coordinate(Angle):
+
+    def __init__(self,
+                 coord_src: Union[str, float, int],
+                 coord_type: Union[str, float, int],
+                 coord_label: str = None) -> None:
+        Angle.__init__(self)
+        self._coord_src = coord_src
+        self._coord_type = coord_type
+        self._coord_label = coord_label
+        self._coord_dd = Coordinate.convert_to_dd(self._coord_src, self._coord_type, self._coord_label)
+        if self._coord_dd is None:
+            if coord_label:
+                raise ValueError(f'{coord_label} incorrect or unsupported format.\n')
+            else:
+                raise ValueError(f'{coord_type} incorrect or unsupported format.\n')
+
+    def __str__(self):
+        return self._coord_src
+
+    @property
+    def coord_dd(self):
+        return self._coord_dd
+
+    @property
+    def coord_src(self):
+        return self._coord_src
+
+    @staticmethod
+    def convert_compacted_to_dd(coord_src: str, ang_type: str) -> Union[float, None]:
+        """ Converts DMSH or HDMS format into DD format
+        :param coord_src: str
+        :param ang_type: str
+        :return: float: angle in decimal degrees format, if conversion failed (not supported format,
+                 error in angle example minutes >= 60, incorrect type - returns None)
+        >>> assert Coordinate.convert_compacted_to_dd('1800000E', AT_LONGITUDE) == 180.0
+        >>> assert Coordinate.convert_compacted_to_dd('1800000.0W', AT_LONGITUDE) == -180.0
+        >>> assert Coordinate.convert_compacted_to_dd('E1800000', AT_LONGITUDE) == 180.0
+        >>> assert Coordinate.convert_compacted_to_dd('W1800000.0', AT_LONGITUDE) == -180.0
+        >>> assert Coordinate.convert_compacted_to_dd('W0003000.000', AT_LONGITUDE) == -0.5
+        >>> assert Coordinate.convert_compacted_to_dd('E1234601.445', AT_LONGITUDE) == 123.76706805555555
+        >>> assert Coordinate.convert_compacted_to_dd('E1810000', AT_LONGITUDE) is None
+        >>> assert Coordinate.convert_compacted_to_dd('S200000.00', AT_LONGITUDE) is None
+        >>> assert Coordinate.convert_compacted_to_dd('900000N', AT_LATITUDE) == 90.0
+        >>> assert Coordinate.convert_compacted_to_dd('900000.0S', AT_LATITUDE) == -90.0
+        >>> assert Coordinate.convert_compacted_to_dd('234601.445N', AT_LATITUDE) == 23.767068055555555
+        >>> assert Coordinate.convert_compacted_to_dd('N900000', AT_LATITUDE) == 90.0
+        >>> assert Coordinate.convert_compacted_to_dd('S900000.0', AT_LATITUDE) == -90.0
+        >>> assert Coordinate.convert_compacted_to_dd('N003000.000', AT_LATITUDE) == 0.5
+        >>> assert Coordinate.convert_compacted_to_dd('S900000.001', AT_LATITUDE) is None
+        >>> assert Coordinate.convert_compacted_to_dd('E0900000.0', AT_LATITUDE) is None
+        >>> assert Coordinate.convert_compacted_to_dd('18000.00E', AT_LONGITUDE) == 180.0
+        >>> assert Coordinate.convert_compacted_to_dd('18000.0000W', AT_LONGITUDE) == -180.0
+        >>> assert Coordinate.convert_compacted_to_dd('E04750.222', AT_LONGITUDE) == 47.83703333333333
+        >>> assert Coordinate.convert_compacted_to_dd('W18010.000', AT_LONGITUDE) is None
+        >>> assert Coordinate.convert_compacted_to_dd('S9010.000', AT_LONGITUDE) is None
+        >>> assert Coordinate.convert_compacted_to_dd('9000.00S', AT_LATITUDE) == -90.0
+        >>> assert Coordinate.convert_compacted_to_dd('9000.0000N', AT_LATITUDE) == 90.0
+        >>> assert Coordinate.convert_compacted_to_dd('S4750.222', AT_LATITUDE) == -47.83703333333333
+        >>> assert Coordinate.convert_compacted_to_dd('N9010.000', AT_LATITUDE) is None
+        >>> assert Coordinate.convert_compacted_to_dd('E09010.000', AT_LATITUDE) is None
+        """
+        dd = None
+
+        for coord_type, coord_pattern in COORDINATE_COMPACTED[ang_type].items():
+            if coord_pattern.match(coord_src):
+                if coord_type in ['DMSH_COMPACTED', 'HDMS_COMPACTED']:
+                    dmsh_parts = coord_pattern.search(coord_src)
+                    d = int(dmsh_parts.group('deg'))
+                    m = int(dmsh_parts.group('min'))
+                    s = float(dmsh_parts.group('sec'))
+                    h = dmsh_parts.group('hem')
+                    dd = Coordinate.dmsh_parts_to_dd((d, m, s, h))
+                elif coord_type in ['DMH_COMPACTED', 'HDM_COMPACTED']:
+                    dmh_parts = coord_pattern.search(coord_src)
+                    d = int(dmh_parts.group('deg'))
+                    m = float(dmh_parts.group('min'))
+                    h = dmh_parts.group('hem')
+                    dd = Coordinate.dmh_parts_to_dd((d, m, h))
+
+        if dd:
+            if Coordinate.is_angle_within_range(dd, ang_type):
+                return dd
+
+    @staticmethod
+    def convert_to_dd(coord_src: Union[str, float, int],
+                      coord_type: str,
+                      coord_label: str = None) -> Union[float, None]:
+        """ Convert coordinate into DD format.
+        If source value can't be converted (not supported format, incorrect values) exception will be thrown
+        :param coord_src: str, coordinate to be converted
+        :param coord_type: str, AT_LONGITUDE or AT_LATITUDE
+        :param coord_label: str, label a coordinate,  example: 'Reference latitude', 'Circle center latitude'
+        :return: float: coordinate in DD
+        """
+        if type(coord_src) in [float, int]:
+            Coordinate.is_angle_within_range(coord_src, coord_type)
+
+        if not Coordinate.normalize_angle(coord_src):
+            if coord_label:
+                raise ValueError(f'{coord_label} is empty.\n')
+            else:
+                raise ValueError(f'{coord_type} is empty.\n')
+
+        return Coordinate.convert_compacted_to_dd(coord_src, coord_type)
